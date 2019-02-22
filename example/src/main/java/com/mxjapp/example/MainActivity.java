df7e1f486b30ae30aa897334b1af520b32ebe0a7
Jason Ran <ranchengcheng@linglie.com>
2019-02-22 17:06:50 +0800

    no message

--- example/src/main/java/com/mxjapp/example/MainActivity.java
+++ example/src/main/java/com/mxjapp/example/MainActivity.java
@@ -2,9 +2,12 @@ package com.mxjapp.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
-
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
